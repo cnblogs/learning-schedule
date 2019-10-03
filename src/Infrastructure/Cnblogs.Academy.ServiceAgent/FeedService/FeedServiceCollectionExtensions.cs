536e8aa6ecf110b5c948d941386e7a2e0a9cd72b
keke <fengxxxke@gmail.com>
2019-10-03 10:34:14 +0800

    Configure IFeedsApi

--- src/Infrastructure/Cnblogs.Academy.ServiceAgent/FeedService/FeedServiceCollectionExtensions.cs
+++ src/Infrastructure/Cnblogs.Academy.ServiceAgent/FeedService/FeedServiceCollectionExtensions.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Cnblogs.Academy.ServiceAgent.FeedsApi;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cnblogs.Academy.ServiceAgent.FeedService
 {
    public static class FeedServiceCollectionExtensions
     {
-        public static IServiceCollection AddFeedService(this IServiceCollection services)
+        public static IServiceCollection AddFeedService(this IServiceCollection services, string uri = "http://feeds_api")
         {
+            services.AddHttpClient("feeds_api",x=>{
+                x.BaseAddress = new Uri(uri);
+            }).AddTypedClient(x=>Refit.RestService.For<IFeedsApi>(x))
+            .Services.AddTransient<IFeedServiceAgent, FeedServiceAgent>();
             return services;
         }
     }
